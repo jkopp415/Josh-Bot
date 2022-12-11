b0669101de08bb11681d2b5d6e0053e0965056dc
Joshua Kopp <kopp.josh415@gmail.com>
2022-12-11 15:01:49 -0500

    Added functionality for slash commands

--- src/main/java/com/koppj/joshbot/BotCommands.java
+++ src/main/java/com/koppj/joshbot/BotCommands.java
@@ -1,10 +1,7 @@
 package com.koppj.joshbot;
 
 import java.io.InputStream;
-import net.dv8tion.jda.api.entities.Message;
-import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
-import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import net.dv8tion.jda.api.utils.FileUpload;
@@ -13,6 +10,7 @@ public class BotCommands extends ListenerAdapter
 {
 	public BotCommands()
 	{
+		// The list of commands that the bot will have
 		JoshBot.getJoshBot().updateCommands().addCommands(
 				Commands.slash("joshping", "Pong!"),
 				Commands.slash("joshshutdown", "Shuts down the bot (if you're jkopp415)"),
@@ -23,18 +21,32 @@ public class BotCommands extends ListenerAdapter
 	@Override
 	public void onSlashCommandInteraction(SlashCommandInteractionEvent event)
 	{
+		// Ping command
 		if (event.getName().equals("joshping"))
 			event.reply("Pong!").queue();
 		
+		// Shutdown bot command
 		if (event.getName().equals("joshshutdown"))
 		{
+			// If the user is not the dev, don't let them shut the bot down
+			if (!event.getUser().getName().equals(JoshBot.getDevName()))
+			{
+				event.reply("You don't have the right permissions, stop it.").setEphemeral(true).queue();
+				return;
+			}
+		
+			// Replies to the user so the response isn't empty
+			event.reply("Shutting down...").setEphemeral(true).queue();
+			
+			// Sends the shutdown message
 			InputStream goodbyeCatStream = JoshBot.loadAsset("assets/goodbye_cat.jpg");
 			event.getChannel().sendMessage("Shutting down. Bye!")
 				.addFiles(FileUpload.fromData(goodbyeCatStream, "goodbye_cat.jpg")).queue();
 			JoshBot.shutdownBot();
 		}
 		
+		// Space fact command
 		if (event.getName().equals("joshspacefact"))
-			event.reply(SpaceFactsBot.getRandomSpaceFact()).queue();
+			event.reply(SpaceFacts.getRandomSpaceFact()).queue();
 	}
 }
--- src/main/java/com/koppj/joshbot/JoshBot.java
+++ src/main/java/com/koppj/joshbot/JoshBot.java
@@ -6,11 +6,7 @@ import org.apache.commons.configuration2.Configuration;
 import org.apache.commons.configuration2.builder.fluent.Configurations;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
-import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
-import net.dv8tion.jda.api.hooks.ListenerAdapter;
-import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import net.dv8tion.jda.api.requests.GatewayIntent;
-import net.dv8tion.jda.api.utils.FileUpload;
 
 public class JoshBot
 {
@@ -22,7 +18,6 @@ public class JoshBot
 	private static boolean devMode;							// The developer mode flag
 	private static String devName;							// The developer's username
 	private static boolean enableComeBot;					// The Come Bot flag
-	private static boolean enableSpaceFactsBot;				// The Space Facts Bot flag
 	private static boolean enableBotPing;					// The bot ping flag
 	
 	private static JDA joshBot;
@@ -38,14 +33,12 @@ public class JoshBot
 			System.out.println("config.properties not found.");
 		}
 		
-		
 		// Read in some of the basic configs
 		token = config.getString("joshbot.token");
 		userId = config.getString("joshbot.userid");
 		devMode = config.getBoolean("joshbot.devmode");
 		devName = config.getString("joshbot.devname");
 		enableComeBot = config.getBoolean("joshbot.enablecomebot");
-		enableSpaceFactsBot = config.getBoolean("joshbot.enablespacefactsbot");
 		enableBotPing = config.getBoolean("joshbot.enablebotping");
 		
 		// Create the bot with the token, giving it the MESSAGE_CONTENT intent
@@ -53,13 +46,11 @@ public class JoshBot
 				.enableIntents(GatewayIntent.MESSAGE_CONTENT)
 				.build();
 		
+		// Adds a list of commands to the bot
 		joshBot.addEventListener(new BotCommands());
 		
 		// Enable Come Bot based on the flag in the config
 		if (enableComeBot) joshBot.addEventListener(new ComeBot());
-		
-		// Enable Space Facts Bot based on the flag in the config
-		if (enableSpaceFactsBot) joshBot.addEventListener(new SpaceFactsBot());
 		
 		// Enable bot ping based on the flag in the config
 		if (enableBotPing) joshBot.addEventListener(new BotPing());
