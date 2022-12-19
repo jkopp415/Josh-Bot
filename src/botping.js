7a3ee2564f5969ac05f95d05179748188907b930
Josh K. | IREC Payloads Manager <kopp.josh415@knights.ucf.edu>
2022-12-19 18:24:07 -0500

    Deleted unnecessary files

--- src/botping.js
+++
@@ -1,8 +0,0 @@
-// Import nodejs file functions
-const path = require('path');
-
-// The gif that the bot uses to react to the user
-const whoPingedMeGif = path.join(__dirname, '/assets/who_pinged_me.gif');
-
-// Export consts/functions to index.js
-module.exports = { whoPingedMeGif };
--- src/botshutdown.js
+++
@@ -1,8 +0,0 @@
-// Import nodejs file functions
-const path = require('path');
-
-// The image that the bot sends before being shut down
-const goodbyeCat = path.join(__dirname, '/assets/goodbye_cat.jpg');
-
-// Export consts/functions to index.js
-module.exports = { goodbyeCat };
